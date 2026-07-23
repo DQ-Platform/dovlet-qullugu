adec4e7d0d37ac8b7b587093777fe9760d57d6ec
Dq-Platform <agasifimanov@gmail.com>
2026-07-23 08:39:39 +0400

    Refactor test system to show questions and results

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-function startTest() {
-    alert("Onlayn test sistemi tezliklə istifadəyə veriləcək!");
+let cariSual = 0;
+let bal = 0;
+
+const sualElement = document.getElementById("sual");
+const sayElement = document.getElementById("say");
+const neticeElement = document.getElementById("netice");
+const butonlar = document.querySelectorAll("#variantlar button");
+
+function sualiGoster() {
+
+    if (cariSual >= suallar.length) {
+        document.getElementById("variantlar").style.display = "none";
+        sualElement.innerHTML = "🎉 Test tamamlandı!";
+        sayElement.innerHTML = "";
+        neticeElement.innerHTML =
+        "Nəticəniz: <b>" + bal + " / " + suallar.length + "</b>";
+        return;
+    }
+
+    const s = suallar[cariSual];
+
+    sayElement.innerHTML = (cariSual + 1) + " / " + suallar.length;
+    sualElement.innerHTML = s.sual;
+
+    for (let i = 0; i < 4; i++) {
+        butonlar[i].innerHTML = s.variantlar[i];
+
+        butonlar[i].onclick = function () {
+
+            if (s.variantlar[i] === s.cavab) {
+                bal++;
+            }
+
+            cariSual++;
+            sualiGoster();
+
+        };
+    }
+
 }
+
+sualiGoster();
